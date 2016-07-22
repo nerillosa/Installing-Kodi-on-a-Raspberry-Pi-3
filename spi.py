4994b37176e9e5548c0bc497809f7b87071631be
Neri Llosa <nerillosa@gmail.com>
2016-07-22 16:59:24 -0700

    Add files via upload

--- spi.py
+++ spi.py
@@ -5,24 +5,22 @@ import time
 
 spi = spidev.SpiDev() # create spi object
 spi.open(0, 0) # open spi port 0, device (CS) 0
+frame = [0, 0] # initialize list to be sent to spi bus
+
 try:
 	while True:
 		for num in range(45,127):
-        		frame = bytearray()
-        		frame.append(0)
-        		frame.append(num)
+        		frame[1]=num
 			resp = spi.xfer2(frame)
-			time.sleep(0.05) # sleep for 0.05 seconds
+			time.sleep(0.05) # sleep for 0.1 seconds
 		#end for
         
 		time.sleep(0.5) # sleep for 0.5 seconds
 
         	for num in range(127,45, -1):
-                	frame = bytearray()
-                	frame.append(0)
-                	frame.append(num)
+        		frame[1]=num
                 	resp = spi.xfer2(frame)
-                	time.sleep(0.05) # sleep for 0.05 seconds
+                	time.sleep(0.05) # sleep for 0.1 seconds
         	#end for
 	#end While
 
