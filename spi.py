775306db699df98f80bfc1bf7f0bdc75a1ab38fe
Neri Llosa <nerillosa@gmail.com>
2016-07-23 07:23:10 -0700

    Update spi.py

--- spi.py
+++ spi.py
@@ -5,22 +5,23 @@ import time
 
 spi = spidev.SpiDev() # create spi object
 spi.open(0, 0) # open spi port 0, device (CS) 0
-frame = [0, 0] # initialize list to be sent to spi bus
+frame = bytearray([0,0]) # The first byte is the control byte and in this application is always \x00
 
+#the following while loop will assign values to the second byte of the frame sent to SPI bus
 try:
 	while True:
 		for num in range(45,127):
-        		frame[1]=num
+        		frame[1] = num
 			resp = spi.xfer2(frame)
-			time.sleep(0.05) # sleep for .05 seconds (1/20th of a second)
+			time.sleep(0.05) # sleep for 0.05 seconds (1/20)
 		#end for
         
 		time.sleep(0.5) # sleep for 0.5 seconds
 
         	for num in range(127,45, -1):
-        		frame[1]=num
+        		frame[1] = num
                 	resp = spi.xfer2(frame)
-                	time.sleep(0.05)
+                	time.sleep(0.05) # sleep for 0.05 seconds
         	#end for
 	#end While
 
